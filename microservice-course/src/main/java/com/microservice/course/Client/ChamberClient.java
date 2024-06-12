44c9fcedd43e2be209effe596e1fe5813e8b8d88
KrlosPK <centille095@gmail.com>
2024-06-12 17:24:02 -0500

    Add config server microservice

--- microservice-course/src/main/java/com/microservice/course/Client/ChamberClient.java
+++ microservice-course/src/main/java/com/microservice/course/Client/ChamberClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-chamber", url = "localhost:8090/api/chamber")
+@FeignClient(name = "msvc-chamber", url = "localhost:8080/api/chamber")
 public interface ChamberClient {
 
     @GetMapping("/search-by-course/{idCourse}")
